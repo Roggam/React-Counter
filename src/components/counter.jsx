cfa6a1a3a4185250ccb52660008c190e4cb10b00
Rogelio Gamboa Jr <roggam@yahoo.com.mx>
2022-01-05 11:55:37 -0500

    learning more about react states

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,36 +1,34 @@
 import React, { Component } from 'react'
 
 
-class Counter extends React.Component {
-    state = {
-        count: 0,
-tags: ['tag1', 'tag2', 'tag3']
-    };
+class Counter extends Component {
+ 
+ 
 
 
     render() {
-      
+
         return (
             <div>
-                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button className='btn btn-secondary btn-sm'>Increment</button>
-                <ul>
-                  {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-                </ul>
+        
+                <span className={this.getBadgeClasses()}> {this.formatCount()} </span>
+                <button onClick={() => this.props.onIncrement(this.props.counter)} className='btn btn-secondary btn-sm'>Increment</button>
+                <button className="btn btn-danger btn-sm m-2" onClick={() => this.props.onDelete(this.props.counter.id)}>Delete</button>
             </div>
         );
 
 
     }
+
     getBadgeClasses() {
         let classes = "badge  m-2 badge-";
-        classes += (this.state.count === 0) ? 'warning' : 'primary';
+        classes += (this.props.counter.value === 0) ? 'warning' : 'primary';
         return classes;
     }
 
     formatCount() {
-        const { count } = this.state;
-        return count === 0 ? 'Zero' : count;
+        const { value } = this.props.counter;
+        return value === 0 ? 'Zero' : value;
     }
 
 }

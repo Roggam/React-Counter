9a247021ee8e3908e09b418d8463cf85a6472d8d
Rogelio Gamboa Jr <roggam@yahoo.com.mx>
2022-02-16 17:05:23 -0500

    learning more about props

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -5,6 +5,7 @@ import Counter from './counter'
 const Counters = ({onReset, onDelete, onIncrement, counters, onDecrement}) => {
     return ( 
     <div>
+        
                 <button className="btn btn-primary btn-sm m-2"
                     onClick={onReset}>
                     Reset</button>
